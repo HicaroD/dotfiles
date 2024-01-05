c5f2e00c871bb61c0abfebf1b8479069c24bd133
HicaroD <hdanrlley1@gmail.com>
2024-01-05 13:18:11 -0300

    script: command for configuring monitor to 165Hz

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,5 @@
 # Disabling natural scrolling
 echo "Disabling mouse natural scrolling"
 xinput set-prop 10 292 0
+echo "Setting monitor to 165Hz"
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165
