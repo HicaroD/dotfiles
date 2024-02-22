a85d89fd127cf37234e662aa0eda629b3579066e
HicaroD <hdanrlley1@gmail.com>
2024-02-22 09:33:46 -0300

    fix(scripts): adding command for identifying the mouse ID

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 # Disabling natural scrolling
 echo "Disabling mouse natural scrolling"
-xinput set-prop 10 292 0
+xinput set-prop $(xinput list --id-only "pointer:USB Gaming Mouse") 292 0
 echo "Setting monitor to 165Hz"
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165
