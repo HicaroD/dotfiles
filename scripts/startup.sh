3c93b6ca2ee8c00f8e0e8d15fbce214e4fdd5806
HicaroD <hdanrlley1@gmail.com>
2024-06-12 17:20:48 -0300

    fix: updating startup script for setting up monitor

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,4 +2,4 @@
 echo "Disabling mouse natural scrolling"
 xinput set-prop $(xinput list --id-only "pointer:USB Gaming Mouse") 292 0
 echo "Setting monitor to 165Hz"
-xrandr --output DisplayPort-0 --primary --mode 1920x1080 --rate 165
+xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.0
