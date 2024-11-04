38bc7c2bf42b9ace5fd8b190bb4ae01def095ed4
HicaroD <hdanrlley1@gmail.com>
2024-11-04 12:16:09 -0300

    fix(scripts): changing screen ouput name and echo command

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 # Disabling natural scrolling
 echo "Disabling mouse natural scrolling"
 xinput set-prop $(xinput list --id-only "pointer:USB Gaming Mouse") 292 0
-echo "Setting monitor to 165Hz"
-xrandr --output HDMI-1 --primary --mode 1920x1080 --rate 144.0
+echo "Setting monitor to 144Hz"
+xrandr --output HDMI-A-0 --primary --mode 1920x1080 --rate 144.0
